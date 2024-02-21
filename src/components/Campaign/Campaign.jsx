3f576037aa6a071109dc6ebe42ce3a4cf2a67932
youhyeoneee <khukyh2018@khu.ac.kr>
2024-02-21 23:33:12 +0900

    feat: 캠페인 컴포넌트 추가

---
+++ src/components/Campaign/Campaign.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import {
+  Badge,
+  Card,
+  CardBody,
+  CardFooter,
+  CardImg,
+  CardText,
+  CardTitle,
+  Col,
+  Container,
+} from "react-bootstrap";
+
+export default function Campaign({ campaign }) {
+  return (
+    <Col>
+      <Card className="h-100" style={{ border: "none" }}>
+        <CardImg src={campaign.img} alt="..." position="top" />
+        <CardBody
+          style={{ display: "flex", flexDirection: "column", gap: "16px" }}
+        >
+          <div>
+            <CardTitle
+              style={{
+                fontSize: "18px",
+                fontWeight: "700",
+                lineHeight: 1.5,
+                color: "#00c4c4",
+              }}
+            >
+              {parseInt(campaign.achievementRate / 10)}% 달성
+            </CardTitle>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                gap: "4px",
+              }}
+            >
+              <CardText
+                as="div"
+                style={{
+                  fontSize: "16px",
+                  fontWeight: "400",
+                  lineHeight: 1.5,
+                }}
+              >
+                {campaign.title}
+              </CardText>
+              <CardText
+                style={{
+                  fontSize: "12px",
+                  fontWeight: "400",
+                  lineHeight: "1.5",
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                  whiteSpace: "nowrap",
+                  margin: "0",
+                  color: "#868e96",
+                }}
+              >
+                {campaign.nickName}
+              </CardText>
+            </div>
+          </div>
+          <div
+            style={{
+              border: "none",
+              backgroundColor: "#ffffff",
+            }}
+          >
+            <Badge
+              bg="#868e96"
+              style={{
+                border: "solid 1px #f2f4f6",
+                backgroundColor: "#f2f4f6",
+                color: "#495057",
+                fontWeight: "500",
+              }}
+            >
+              {campaign.totalBackedAmount}원
+            </Badge>
+          </div>
+        </CardBody>
+      </Card>
+    </Col>
+  );
+}
--- src/pages/page.jsx
+++ src/pages/page.jsx
@@ -1,13 +1,28 @@
-import React, { useEffect } from "react";
-import { Container } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Row } from "react-bootstrap";
+import Campaign from "~/components/Campaign/Campaign";
+import MyNavbar from "~/components/MyNavbar/MyNavbar";
 
 export default function MainPage() {
+  const [campaignList, setCampaignList] = useState([
+    {
+      img: "https://cdn1.wadiz.kr/images/20240214/1707890500517.png/wadiz/optimize",
+      title:
+        "[하루 15분] 무의식 브레인 해킹 영어! 나이 상관없이 영어뇌 장착하기",
+      nickName: "블래어",
+      achievementRate: 23161,
+      totalBackedAmount: 115805000,
+    },
+  ]);
   return (
-    <Container className="min-vh-100">
+    <>
       <div>
-        <h1>This is my MainPage</h1>
-        <p>MainPage 입니다.</p>
+        <Row className="row-cols-1 row-cols-md-4 g-4">
+          {campaignList.map((campaign) => (
+            <Campaign campaign={campaign} key={campaign.title} />
+          ))}
+        </Row>
       </div>
-    </Container>
+    </>
   );
 }
