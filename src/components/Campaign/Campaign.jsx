775944f6a1ac09c3698981e4c4a0c2b583de4190
youhyeoneee <khukyh2018@khu.ac.kr>
2024-02-23 00:25:00 +0900

    design: 금액 콤마 및 단위 적용

--- src/components/Campaign/Campaign.jsx
+++ src/components/Campaign/Campaign.jsx
@@ -12,9 +12,28 @@ import {
 } from "react-bootstrap";
 
 export default function Campaign({ campaign }) {
+  const formatLargeNumber = (n) => {
+    if (n < 1e3) return n.toLocaleString("ko-KR");
+    if (n >= 1e3 && n < 1e4)
+      return Math.floor(n / 1e3).toLocaleString("ko-KR") + "천";
+    if (n >= 1e4 && n < 1e8)
+      return Math.floor(n / 1e4).toLocaleString("ko-KR") + "만";
+    if (n >= 1e8 && n < 1e12)
+      return (Math.floor((n * 10) / 1e8) / 10).toLocaleString("ko-KR") + "억";
+    if (n >= 1e12) return +(n / 1e12).toFixed(1).toLocaleString("ko-KR") + "조";
+  };
+
   return (
     <Col>
-      <Card className="h-100" style={{ border: "none" }}>
+      <Card
+        className="h-100"
+        style={{
+          border: "none",
+          display: "flex",
+          flexDirection: "column",
+          gap: "8px",
+        }}
+      >
         <CardImg src={campaign.photoUrl} alt="..." position="top" />
         <CardBody
           style={{
@@ -22,52 +41,51 @@ export default function Campaign({ campaign }) {
             flexDirection: "column",
             gap: "16px",
             justifyContent: "space-between",
-            padding: "var(--bs-card-spacer-y) 0",
+            padding: "0",
           }}
         >
-          <div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "4px",
+            }}
+          >
             <CardTitle
               style={{
+                marginBottom: "0",
                 fontSize: "18px",
                 fontWeight: "700",
                 lineHeight: 1.5,
                 color: "#00c4c4",
               }}
             >
-              {parseInt(campaign.achievementRate / 10)}% 달성
+              {campaign.achievementRate.toLocaleString("ko-KR")} 달성
             </CardTitle>
-            <div
+            <CardText
+              as="div"
               style={{
-                display: "flex",
-                flexDirection: "column",
-                gap: "4px",
+                fontSize: "16px",
+                fontWeight: "400",
+                lineHeight: 1.5,
               }}
             >
-              <CardText
-                as="div"
-                style={{
-                  fontSize: "16px",
-                  fontWeight: "400",
-                  lineHeight: 1.5,
-                }}
-              >
-                {campaign.title}
-              </CardText>
-              <CardText
-                style={{
-                  fontSize: "12px",
-                  fontWeight: "400",
-                  lineHeight: "1.5",
-                  overflow: "hidden",
-                  textOverflow: "ellipsis",
-                  whiteSpace: "nowrap",
-                  margin: "0",
-                  color: "#868e96",
-                }}
-              >
-                {campaign.nickName}
-              </CardText>
-            </div>
+              {campaign.title}
+            </CardText>
+            <CardText
+              style={{
+                fontSize: "12px",
+                fontWeight: "400",
+                lineHeight: "1.5",
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+                whiteSpace: "nowrap",
+                margin: "0",
+                color: "#868e96",
+              }}
+            >
+              {campaign.nickName}
+            </CardText>
           </div>
           <div
             style={{
@@ -81,10 +99,11 @@ export default function Campaign({ campaign }) {
                 border: "solid 1px #f2f4f6",
                 backgroundColor: "#f2f4f6",
                 color: "#495057",
+                fontSize: "10px",
                 fontWeight: "500",
               }}
             >
-              {campaign.totalBackedAmount}원
+              {formatLargeNumber(campaign.totalBackedAmount)} 원+
             </Badge>
           </div>
         </CardBody>
